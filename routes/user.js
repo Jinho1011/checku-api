375800f5ff2c602959ee01f28796167ab46b4f44
jinho1011 <gino9940@gmail.com>
2021-07-02 00:16:03 +0900

    feat: add onload, load

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,17 @@
 var express = require("express");
 var router = express.Router();
 var axios = require("axios");
+var qs = require("qs");
 
-router.get("/onload", function (req, res, next) {
+router.get("/onload", async function (req, res, next) {
+  const JSESSIONID = req.cookies.JSESSIONID;
+
   var config = {
     method: "post",
     url: "https://kuis.konkuk.ac.kr/Main/onLoad.do",
     headers: {
       Referer: "https://kuis.konkuk.ac.kr/index.do",
-      Cookie: `JSESSIONID=${req.cookies.JSESSIONID}`,
+      Cookie: `JSESSIONID=${JSESSIONID}`,
     },
   };
 
@@ -21,8 +24,60 @@ router.get("/onload", function (req, res, next) {
     });
 });
 
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+router.get("/load", async function (req, res, next) {
+  var data = "_AUTH_MENU_KEY=1122208&";
+  const JSESSIONID = req.cookies.JSESSIONID;
+
+  var config = {
+    method: "post",
+    url: "https://kuis.konkuk.ac.kr/RegiRegisterMasterInq/load.do",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+      Referer: "https://kuis.konkuk.ac.kr/index.do",
+      Cookie: `JSESSIONID=${JSESSIONID}`,
+    },
+    data: data,
+  };
+
+  axios(config)
+    .then(function (response) {
+      res.json(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
+router.get("/", async function (req, res, next) {
+  const JSESSIONID = req.cookies.JSESSIONID;
+
+  const strStdNo = "202011361";
+  const strStdNm = "%EC%A0%84%EC%A7%84%ED%98%B8"; // encodeURI
+  const strYy = "2021";
+  const strShtm = "B01011";
+  const strDeptCd = "127428";
+  const strUserId = "202011361";
+
+  var data = `_AUTH_MENU_KEY=1122208&%40d1%23strStdNo=${strStdNo}&%40d1%23strStdNm=${strStdNm}&%40d1%23strYy=${strYy}&%40d1%23strShtm=${strShtm}&%40d1%23strSust=&%40d1%23strDeptCd=${strDeptCd}&%40d1%23strUserId=${strUserId}&%40d1%23strDeptGrd=0&%40d1%23strMenuCd=1122208&%40d%23=%40d1%23&%40d1%23=dmParam&%40d1%23tp=dm&`;
+
+  var config = {
+    method: "post",
+    url: "https://kuis.konkuk.ac.kr/RegiRegisterMasterInq/find.do",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+      Referer: "https://kuis.konkuk.ac.kr/index.do",
+      Cookie: `JSESSIONID=${JSESSIONID}`,
+    },
+    data: data,
+  };
+
+  axios(config)
+    .then(function (response) {
+      res.json(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 });
 
 module.exports = router;
