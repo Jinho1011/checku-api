cee7d5ac452001dabb63d110bac3789fc9bfbb5a
jinho1011 <gino9940@gmail.com>
2021-07-02 00:52:24 +0900

    feat: add grade onload route

--- routes/grade.js
+++ routes/grade.js
@@ -6,6 +6,28 @@ var qs = require("qs");
 // 전체 성적
 router.get("/", async function (req, res, next) {});
 
+router.get("/load", async function (req, res, next) {
+  var config = {
+    method: "post",
+    url: "https://kuis.konkuk.ac.kr/GradNowShtmGradeInq/load.do",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+      Referer: "https://kuis.konkuk.ac.kr/index.do",
+      Cookie:
+        "JSESSIONID=00045MMGq4Whx32pFTVTfIDeFYe:2LL3FQPNS4:17BGRNS49U; WMONID=gghluu6ziW3",
+    },
+    data: data,
+  };
+
+  axios(config)
+    .then(function (response) {
+      res.json(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
 // 현재 학기
 router.get("/now", async function (req, res, next) {});
 
