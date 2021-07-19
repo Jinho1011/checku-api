25a1c70043bc92366469df9e8eb461c4445dcbf4
jinho1011 <gino9940@gmail.com>
2021-07-19 12:33:28 +0900

    add getCorsYy

--- routes/graduate.js
+++ routes/graduate.js
@@ -3,27 +3,54 @@ var router = express.Router();
 var axios = require("axios");
 var qs = require("qs");
 
-const getCorsYy = (JSESSIONID) => {
-  return "2020";
-};
-
-// 졸업 시뮬레이션
-router.get("/", async function (req, res, next) {
-  const JSESSIONID = req.cookies.JSESSIONID;
-  const corsYy = getCorsYy(JSESSIONID);
-
+const getCorsYy = (JSESSIONID, strStdNo) => {
   var data = qs.stringify({
     _AUTH_MENU_KEY: "1170201",
-    "@d1#strStdNo": req.body.strStdNo ?? "",
-    "@d1#corsYy": corsYy,
+    "@d1#stdNo": strStdNo,
     "@d#": "@d1#",
-    "@d1#": "dmParam2",
+    "@d1#": "dmParam",
     "@d1#tp": "dm",
   });
 
   var config = {
     method: "post",
-    url: "https://kuis.konkuk.ac.kr/RegiRegisterMasterInq/find.do",
+    url: "https://kuis.konkuk.ac.kr/GrdtStdSimul/findSearch.do",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+      Referer: "https://kuis.konkuk.ac.kr/index.do",
+      Cookie: `JSESSIONID=${JSESSIONID}`,
+    },
+    data: data,
+  };
+
+  return axios(config)
+    .then(function (response) {
+      return response.data.DS_CORSYY[0].MIN_YY;
+    })
+    .catch(function (error) {
+      return "";
+    });
+};
+
+// 졸업 시뮬레이션
+router.get("/", async function (req, res, next) {
+  const JSESSIONID = req.cookies.JSESSIONID;
+  const corsYy = await getCorsYy(JSESSIONID, req.body.strStdNo);
+
+  var data = qs.stringify({
+    _AUTH_MENU_KEY: "1170201",
+    "@d1#stdNo": req.body.stdNo ?? "",
+    "@d1#corsYy": corsYy ?? "",
+    "@d#": "@d1#",
+    "@d1#": "dmParam2",
+    "@d1#tp": "dm",
+  });
+
+  console.log(data);
+
+  var config = {
+    method: "post",
+    url: "https://kuis.konkuk.ac.kr/GrdtStdSimul/findSearch2.do",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
       Referer: "https://kuis.konkuk.ac.kr/index.do",
@@ -40,3 +67,5 @@ router.get("/", async function (req, res, next) {
       res.json(error);
     });
 });
+
+module.exports = router;
